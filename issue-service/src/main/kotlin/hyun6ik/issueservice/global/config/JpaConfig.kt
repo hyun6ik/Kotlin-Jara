110a1f0fa06f02037f7f4c0b777aa9d380c4f68d
hyun6ik <fkanwm@naver.com>
2022-09-03 00:18:00 +0900

    feat : Jpa Auditing 설정

---
+++ issue-service/src/main/kotlin/hyun6ik/issueservice/global/config/JpaConfig.kt
@@ -0,0 +1,9 @@
+package hyun6ik.issueservice.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig {
+}
