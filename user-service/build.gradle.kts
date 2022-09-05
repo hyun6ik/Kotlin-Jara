c6f333da6140f7e06f2ebefcbb1b8f32838d5aae
hyun6ik <fkanwm@naver.com>
2022-09-05 16:39:30 +0900

    feat : user-service : build.gradle.kts 의존성 설정

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,19 +1,15 @@
-plugins {
-    id("java")
-}
-
-group = "com.hyun6ik"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+    implementation("at.favre.lib:bcrypt:0.9.0")
+
+    runtimeOnly("io.r2dbc:r2dbc-h2")
+
 }
