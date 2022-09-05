92b1918f6a9d9bcba369a5cab299aded36d58450
hyun6ik <fkanwm@naver.com>
2022-09-05 18:25:39 +0900

    fix: User Column name -> username으로 수정

--- user-service/src/main/resources/scripts/schema.sql
+++ user-service/src/main/resources/scripts/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE users
 (
     id bigint NOT NULL AUTO_INCREMENT,
     email varchar(100),
-    name varchar(50),
+    username varchar(50),
     password varchar(500),
     profile_url varchar(500),
     created_at timestamp default NOW(),
